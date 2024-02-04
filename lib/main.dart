40cde28194207c4d5a6a9a2e385dd00dd767bf49
faisal <fe9lsh3ben@gmail.com>
2024-02-04 14:51:26 +0300

    incloding Dio

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:js_interop';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
-
+import 'package:dio/dio.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +12,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -30,7 +28,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -38,64 +35,56 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- 
-
   void _incrementCounter() async {
-   
     final body = {
       "fullName": {
-        "english":<String> ["Faisal", "Shabeen"],
-        "arabic":"فيصل شعبين"
+        "english": <String>["Faisal", "Shabeen"],
+        "arabic": ["شعبين", "فيصل"]
       },
       "username": "fe9l",
       "age": "30",
       "govermentalID": "1089036089",
-      "birthOfDate": "2015/12/27",
-      "email":"fe9lsh3ben@gmail.com"
-      };
+      "birthOfDate": "1994/12/27",
+      "email": "fe9lsh3ben@gmail.com"
+    };
+
+    final jsonString = json.encode(body);
+    final headers = {HttpHeaders.contentTypeHeader: 'application/json'};
+    final dio = Dio();
+    try {
       
-      final jsonString = json.encode(body);
-      final headers = {HttpHeaders.contentTypeHeader: 'application/json'};
+
       
-      try{
 
-        final uri = Uri.https('127.0.0.1:3050', '/auth&auth');
-        var response = await http.post(uri, headers: headers
-        ,body: jsonString
-        );
-        
-        print(response.body);
-
-      }catch(e){
-        print(e);
-
-      }
+      final response1 = await dio.post('https://127.0.0.1:3050/auth&auth',
+          data: jsonString,
+          options: Options(headers: headers));
       
-  
-
+      print(response1.data);
+      
+    } catch (e) {
+      print(e);
+    } finally{
+      dio.close();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-     
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-  
         title: Text(widget.title),
       ),
       body: Center(
-  
         child: Column(
-        
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
             ),
             Text(
-             'You have pushed the button this many times:',
+              'You have pushed the button this many times:',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
@@ -105,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ),    
-      );
+      ),
+    );
   }
 }
