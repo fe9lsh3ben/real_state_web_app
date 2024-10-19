13bc23d270720a282ca0a29f33bb97c670b7fa38
faisal shaabeen <fe9lsh3ben@gmail.com>
2024-10-19 04:55:04 +0300

    map

--- lib/widgets.dart/map.dart
+++ lib/widgets.dart/map.dart
@@ -1,9 +1,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+// import 'package:latlong2/latlong.dart';
 import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
-import 'package:url_launcher/url_launcher.dart';
+// import 'package:url_launcher/url_launcher.dart';
 
 extension Ex on double {
   double toPrecision(int n) => double.parse(toStringAsFixed(n));
